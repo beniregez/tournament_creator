f40cf954eb688fcda45a85129ea5785bdd2b10e1
Beni Regez <benjamin.regez@gmx.ch>
2025-08-31 23:20:34 +0200

    Check if excel is available before exporting pdfs.

--- src/utils/tourn_to_excel/excel_days_to_pdf.py
+++ src/utils/tourn_to_excel/excel_days_to_pdf.py
@@ -1,6 +1,7 @@
 import os
 import time
 import win32com.client as win32
+import pywintypes
 
 class ExcelToPDFExporter():
     
@@ -15,28 +16,47 @@ class ExcelToPDFExporter():
             time.sleep(1)
         time.sleep(1)
 
-        excel = win32.gencache.EnsureDispatch('Excel.Application')
+        try:
+            excel = win32.gencache.EnsureDispatch('Excel.Application')
+        except pywintypes.com_error as e:
+            print("Excel not installed or unavailable.")
+            print(f"Details: {e}")
+            return
+
         excel.Visible = False  # No Popup
 
-        wb = excel.Workbooks.Open(xlsx_path)
+        try:
+            wb = excel.Workbooks.Open(xlsx_path)
+        except Exception as e:
+            print("Error opening excel file")
+            print(f"Details: {e}")
+            excel.Quit()
+            return
+
 
         pdf_folder = os.path.join(output_dir, "pdfs")
         os.makedirs(pdf_folder, exist_ok=True)
 
         # All sheets in one pdf file
-        sheets = [wb.Sheets(i) for i in range(1, first_n_sheets + 1)]
-        wb.Worksheets([s.Name for s in sheets]).Select()
+        try:
+            sheets = [wb.Sheets(i) for i in range(1, first_n_sheets + 1)]
+            wb.Worksheets([s.Name for s in sheets]).Select()
 
-        total_pdf_path = os.path.abspath(os.path.join(pdf_folder, "tournament_all_days.pdf"))
-        wb.ActiveSheet.ExportAsFixedFormat(0, total_pdf_path)
+            total_pdf_path = os.path.abspath(os.path.join(pdf_folder, "tournament_all_days.pdf"))
+            wb.ActiveSheet.ExportAsFixedFormat(0, total_pdf_path)
 
-        # Every sheet as a single pdf
-        for sheet in sheets:
-            sheet.Select()  # Select only that sheet
-            # Remove invalid characters
-            safe_name = "".join(c for c in sheet.Name if c not in r'\/:*?"<>|')
-            single_pdf_path = os.path.abspath(os.path.join(pdf_folder, f"{safe_name}.pdf"))
-            sheet.ExportAsFixedFormat(0, single_pdf_path)
+            # Every sheet as a single pdf
+            for sheet in sheets:
+                sheet.Select()  # Select only that sheet
+                # Remove invalid characters
+                safe_name = "".join(c for c in sheet.Name if c not in r'\/:*?"<>|')
+                single_pdf_path = os.path.abspath(os.path.join(pdf_folder, f"{safe_name}.pdf"))
+                sheet.ExportAsFixedFormat(0, single_pdf_path)
 
-        wb.Close(False)
-        excel.Quit()
+        except Exception as e:
+            print("Error exporting pdfs")
+            print(f"Details: {e}")
+
+        finally:
+            wb.Close(False)
+            excel.Quit()
