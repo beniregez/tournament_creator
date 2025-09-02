3cee0b38c11545ce827bb4e76c494a76fb60c30d
Beni Regez <benjamin.regez@gmx.ch>
2025-09-02 14:29:57 +0200

    Add icon to window and taskbar.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from controller.controller import Controller
+from PyQt5.QtGui import QIcon
 
 app = QApplication(sys.argv)
+
+app.setWindowIcon(QIcon("C:/Users/benja/Workspace/2025_Tournament_Creator/tournament_creator/src/assets/icon.ico"))
+
 controller = Controller()
 sys.exit(app.exec_())
